1a38246cfda8d9732ac786fd46efcb42e83c24a7
javier <javier_mera88@hotmail.com>
2022-10-14 16:37:13 -0500

    Added class models, db context and migrations.

---
+++ NZWalks.API/NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public sealed class Walk {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty; 
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        // Navigation Properties
+        public Region Region { get; set; } = new Region();
+        public WalkDifficulty WalkDifficulty { get; set; } = new WalkDifficulty();
+    }
+}
---
+++ NZWalks.API/NZWalks.API/Models/Domain/WalkDifficulty.cs
@@ -0,0 +1,7 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public sealed class WalkDifficulty {
+        public Guid Id { get; set; }
+        public string Code { get; set; }
+    }
+}
