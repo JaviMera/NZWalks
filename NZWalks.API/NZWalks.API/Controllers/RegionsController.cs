068cbc85e125b9c174e46c9e8036b24f475c8f17
javier <javier_mera88@hotmail.com>
2022-10-16 11:05:24 -0500

    Begin adding regions controller class

---
+++ NZWalks.API/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            var regions = new List<Region>
+            {
+                new Region{Id = Guid.NewGuid(), Name = "Wellington", Code = "WLG", Area = 227755, Lat = 1231, Long = 232, Population =  500000 },
+                new Region{Id = Guid.NewGuid(), Name = "Auckland", Code = "AUCK", Area = 227755, Lat = 1231, Long = 232, Population =  500000 }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
