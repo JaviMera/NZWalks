6a156bb3f3a69df7beb3ed68da5e3ee9884f604e
javier <javier_mera88@hotmail.com>
2022-10-16 11:42:22 -0500

    Begin adding DTOs

--- NZWalks.API/NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,14 +9,28 @@ namespace NZWalks.API.Controllers
     [Route("[controller]")]
     public class RegionsController : Controller
     {
+        private readonly IRegionsRepository _regionsRepository;
+
+        public RegionsController(IRegionsRepository regionsRepository)
+        {
+            _regionsRepository = regionsRepository;
+        }
+
         [HttpGet]
         public IActionResult GetAllRegions()
         {
-            var regions = new List<Region>
-            {
-                new Region{Id = Guid.NewGuid(), Name = "Wellington", Code = "WLG", Area = 227755, Lat = 1231, Long = 232, Population =  500000 },
-                new Region{Id = Guid.NewGuid(), Name = "Auckland", Code = "AUCK", Area = 227755, Lat = 1231, Long = 232, Population =  500000 }
-            };
+            var regions = _regionsRepository.GetAll()
+                .ToList()
+                .Select(region => new RegionDto
+                {
+                    Area = region.Area,
+                    Code = region.Code,
+                    Id = region.Id,
+                    Lat = region.Lat,
+                    Long = region.Long,
+                    Name = region.Name,
+                    Population = region.Population
+                });
 
             return Ok(regions);
         }
---
+++ NZWalks.API/NZWalks.API/Repositories/IRegionsRepository.cs
@@ -0,0 +1,25 @@
+﻿using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionsRepository
+    {
+        IEnumerable<Region> GetAll();
+    }
+
+    public sealed class RegionsRepository : IRegionsRepository
+    {
+        private readonly NZWalksDbContext _context;
+
+        public RegionsRepository(NZWalksDbContext context)
+        {
+            _context = context; ;
+        }
+
+        public IEnumerable<Region> GetAll()
+        {
+            return _context.Regions.ToList();
+        }
+    }
+}
